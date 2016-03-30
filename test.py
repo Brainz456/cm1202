d8d0093e590cb4379f2edb3e72bf542b15219dbd
Danielh112 <Danielh176@hotmail.co.uk>
2016-03-30 15:05:33 +0100

    Added files via upload
    Test class (Barebones of class)

---
+++ test.py
@@ -0,0 +1,22 @@
+class test:
+
+    #Variables need assigning temp values in place
+    TestID = 0
+    QuestionID = []
+    Topic = ""
+
+
+    def Get_Questions(QuestionID):
+        #form
+
+
+    def Store_answers(StudentID, Results):
+        #Store list
+
+
+    def Check_Answers(Answers, CorrectAnswers):
+        return      #Return Boolean
+
+
+    def Create_Questions(Input):
+
