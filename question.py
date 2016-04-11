a73ccc6f015b0cda7babfc8bb9ab8ebef951d5ed
brainz456 <grantjl@cardiff.ac.uk>
2016-04-11 13:19:38 +0100

    Barebones mk2 uploaded

--- question.py
+++ question.py
@@ -1,14 +1,9 @@
-class question(object):
-	def __init__(self, QuestionID, PossibleAnswers, CorrectAnswer):
-		self.QuestionID = int(QuestionID)
-		self.PossibleAnswers = PossibleAnswers
-		self.CorrectAnswer = str(CorrectAnswer)
+class question:
+	def __init__(self, QuestionID, QuestionAsked, PossibleAnswers, CorrectAnswer):
+         self.questionID = int(QuestionID)
+         self.questionAsked = QuestionAsked
+         self.possibleAnswers = PossibleAnswers
+         self.correctAnswer = str(CorrectAnswer)
+	def __str__(self):
+         return "QuestionID: %d, Question: %s, Possible Answers: %s, Correct Answer: %s" % (self.questionID,self.questionAsked, self.possibleAnswers,self.correctAnswer)
 	
-	def get_ID(self):
-		return self.QuestionID
-	
-	def get_PossibleAnswers(self):
-		return self.PossibleAnswers
-	
-	def get_CorrectAnswer(self):
-		return self.CorrectAnswer
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-class test(object):
-	def __init__(self,TestID,Topic):
+class test:
+    def __init__(self,TestID,Topic):
 		
-		#Variables need assigning temp values in place
-		self.TestID = int(TestID)
-		self.QuestionID = []
-		self.Topic = Topic
-
-
+      #Variables need assigning temp values in place
+      self.testID = int(TestID)
+      self.questions = []
+      self.topic = Topic
+    def __str__(self):
+        return "TestID: %d, Topic: %s" % (self.testID, self.topic)
     def Get_Questions(QuestionID):
         #form
-
-
+        pass
     def Store_answers(StudentID, Results):
         #Store list
-
-
+        pass
     def Check_Answers(Answers, CorrectAnswers):
         return      #Return Boolean
 
-
     def Create_Questions(Input):
-
+        pass
+    
+    def Add_Question(self,QuestionID):
+        self.questions.append(QuestionID)
