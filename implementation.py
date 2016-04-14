16691cbb893dfb9f76c5f7ae30313c56eef7a46f
Daniel Huntley <Danielh176@hotmail.co.uk>
2016-04-15 00:02:32 +0100

    Update to add test and add lesson

--- implementation.py
+++ implementation.py
@@ -19,13 +19,13 @@ class FinalProgram(Tk):
 		#creates a menu at the top of the window
 		menubar = Menu(container)
 		lessonmenu = Menu(menubar, tearoff = 0)
-		lessonmenu.add_command(label="Maths Lesson", command = lambda: self.show_frame(viewMathsLesson))
-		lessonmenu.add_command(label="Architecture Lesson", command = lambda: self.show_frame(viewArchitectureLesson))
-		menubar.add_cascade(label = "Lessons", menu = lessonmenu)
-		testmenu = Menu(menubar, tearoff = 0)
-		testmenu.add_command(label = 'Maths Test', command = lambda: self.show_frame(viewMathsTest))
-		testmenu.add_command(label = 'Architecture Test', command = lambda: self.show_frame(viewArchitectureTest))
-		menubar.add_cascade(label = 'Tests', menu = testmenu)
+		#lessonmenu.add_command(label="Maths Lesson", command = lambda: self.show_frame(viewMathsLesson))
+		#lessonmenu.add_command(label="Architecture Lesson", command = lambda: self.show_frame(viewArchitectureLesson))
+		#menubar.add_cascade(label = "Lessons", menu = lessonmenu)
+		#testmenu = Menu(menubar, tearoff = 0)
+		#testmenu.add_command(label = 'Maths Test', command = lambda: self.show_frame(viewMathsTest))
+		#testmenu.add_command(label = 'Architecture Test', command = lambda: self.show_frame(viewArchitectureTest))
+		#menubar.add_cascade(label = 'Tests', menu = testmenu)
 		menubar.add_command(label = "Log out", command = quit)
 		Tk.config(self, menu = menubar)
 		
@@ -247,8 +247,8 @@ class viewArchitectureTest(Frame):
 
 class addLesson(Frame):
 
-	def __init__(self, parent, controller):
-		Frame.__init__(self, parent)
+	def __init__(self, master, controller):
+		Frame.__init__(self, master)
 		self.controller = controller
 		self.grid()
 
@@ -266,8 +266,7 @@ class addLesson(Frame):
 		contents = Text(self, height=20, width=40)
 		contents.grid(row=3, column = 8)
 
-		butReturn = Button(self, text ='Add Lesson', font= ('MS', 15),
-			command = addLesson)
+		butReturn = Button(self, text ='Add Lesson', font= ('MS', 15), command = self.addLesson)
 		butReturn.grid(row=4, column = 8, pady=10)
 
 		butReturn = Button(self, text ='Return to Test Menu', font= ('MS', 10),
@@ -275,21 +274,66 @@ class addLesson(Frame):
 		butReturn.grid(row=5, column = 7)
 
 	def addLesson(self):
-		controller.show_frame(adminPage)
-		tkinter.messagebox.showinfo("Error", "Invalid Input")
+		self.controller.show_frame(adminPage)
+		tkinter.messagebox.showinfo("Added", "lesson has been added")
+
+
+
+
 
 class addTest(Frame):
 
-	def __init__(self, parent, controller):
-		Frame.__init__(self,parent)
+	def __init__(self, master, controller):
+		Frame.__init__(self, master)
+		self.controller = controller
 		self.grid()
 
 		lblOutput = Label(self, text = 'Add Test', font = ('MS', 25))
 		lblOutput.grid(row=1, column = 7)
 
+		lbTitle = Label(self, text = 'Test Title', font = ('MS', 15))
+		lbTitle.grid(row=2, column = 7)	
+		title = Text(self,width=40, height=1)
+		title.grid(row=2, column = 8)
+
+		question1Title = Label(self, text = 'Question 1', font = ('MS', 15))
+		question1Title.grid(row=3, column = 7)	
+		question1 = Text(self,width=40, height=1)
+		question1.grid(row=3, column = 8)
+
+		question2Title = Label(self, text = 'Question 2', font = ('MS', 15))
+		question2Title.grid(row=3, column = 7)	
+		question2 = Text(self,width=40, height=1)
+		question2.grid(row=3, column = 8)
+
+		question3Title = Label(self, text = 'Question 3', font = ('MS', 15))
+		question3Title.grid(row=4, column = 7)	
+		question3 = Text(self,width=40, height=1)
+		question3.grid(row=4, column = 8)
+
+		question4Title = Label(self, text = 'Question 4', font = ('MS', 15))
+		question4Title.grid(row=5, column = 7)	
+		question4 = Text(self,width=40, height=1)
+		question4.grid(row=5, column = 8)
+
+		question5Title = Label(self, text = 'Question 5', font = ('MS', 15))
+		question5Title.grid(row=6, column = 7)	
+		question5 = Text(self,width=40, height=1)
+		question5.grid(row=6, column = 8)
+
+
+		butReturn = Button(self, text ='Add Test', font= ('MS', 15), command = self.addTest)
+		butReturn.grid(row=7, column = 8, pady=10)
+
 		butReturn = Button(self, text ='Return to Test Menu', font= ('MS', 10),
 			command = lambda: controller.show_frame(adminPage))
-		butReturn.grid(row=4, column = 7)
+		butReturn.grid(row=8, column = 7)
+
+	def addTest(self):
+		self.controller.show_frame(adminPage)
+		tkinter.messagebox.showinfo("Added", "lesson has been added")
+
+
 
 class viewResults(Frame):
 
